e87093dde2882a7d606c99833d89d2aaa0c820ce
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-02-17 21:54:55 -0600

    Errores detectados por flake8

---
+++ Tarea 1/errores_flake8.py
@@ -0,0 +1,5 @@
+from main import *   # Este import tiene el error por el *
+
+def main():  # tiene error de espaciado entre el def y el resto del codigo
+    print("Hola mundo")
+    print("Esta linea es mas larga de lo recomenado por flake8")  # Esta linea es mas larga de lo recomenado por flake8
